c9365a0d7af0356a0ea2b013eb475c45845b436d
Cayo Andrade <>
2021-05-06 01:34:15 +0000

    Structs.c e telas.c feitas

---
+++ structs.c
@@ -0,0 +1,11 @@
+struct sessao {
+  char loginUsuario[20];
+  char horaEntrada[9];
+  char horaSaida[9];
+};
+
+struct usuario {
+  char loginUsuario[20];
+  char senhaUsuario[20];
+};
+
---
+++ telas.c
@@ -0,0 +1,209 @@
+//menu sobre nos
+int menuSobre(void) {
+  int op0;
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                            ***************                              ///\n");
+    printf("///                            ** SOBRE NOS **                              ///\n");
+    printf("///                            ***************                              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Prodik e um programa feito com o intuito de ajudar aqueles que querem  ///\n");
+    printf("///  digitar de forma mais rapida e pratica, com a ajuda de diversos jogos  ///\n");
+    printf("///  e desafios para treinar seus reflexos e habilidades de digitacao.      ///\n");
+    printf("///  Seja para treinar ou apenas se divertir, Prodik podera te ajudar       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                          Pressione 0 para voltar                        ///\n");
+    scanf("%i", &op0);
+    getchar();
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+//menu principal
+int menuPrincipal(void) {
+	int op;
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                               ///\n");
+    printf("///             *************************************             ///\n");
+    printf("///             ** Programa de Digitacao do Kiiush **             ///\n");
+    printf("///             *************************************             ///\n");
+    printf("///                                                               ///\n");
+    printf("///                       1 - Jogar                               ///\n");
+    printf("///                       2 - Configuracoes                       ///\n");
+    printf("///                       3 - Sobre Nos                           ///\n");
+    printf("///                       4 - Placares                            ///\n");    
+    printf("///                       0 - Sair                                ///\n");
+	scanf("%i", &op);
+	getchar();
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+	return op;
+}
+
+//menu de configuracoes
+int menuConfig (void) {
+  int op2;
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                               ///\n");
+    printf("///                      *******************                      ///\n");
+    printf("///                      ** Configuracoes **                      ///\n");
+    printf("///                      *******************                      ///\n");
+    printf("///                                                               ///\n");
+    printf("///                       1 - Conta                               ///\n");
+    printf("///                       2 - Resetar Dados                       ///\n");
+    printf("///                                                               ///\n");
+    printf("///                    Pressione 0 para voltar                    ///\n");
+    scanf("%i", &op2);
+    getchar();
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+  return op2;
+}
+
+//menu da conta
+int menuConta (void) {
+  int op3;
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                               ///\n");
+    printf("///                           ***********                         ///\n");
+    printf("///                           ** Conta **                         ///\n");
+    printf("///                           ***********                         ///\n");
+    printf("///                                                               ///\n");
+    printf("///                       1 - Criar Conta                         ///\n");
+    printf("///                       2 - Entrar em conta                     ///\n");
+    printf("///                                                               ///\n");
+    printf("///                     Pressione 0 para voltar                   ///\n");
+    scanf("%i", &op3);
+    getchar();
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+  return op3;
+}
+
+//menu da criação da conta
+int menuCriarConta (void) {
+	
+	char contaNome[20];
+	char contaSenha[20];
+  int op4;
+	
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                               ///\n");
+    printf("///                        *****************                      ///\n");
+    printf("///                        ** Criar Conta **                      ///\n");
+    printf("///                        *****************                      ///\n");
+    printf("///                                                               ///\n");
+    printf("///                        Digite seu nome (login):               ///\n");
+	scanf("%[A-Z a-z 0-9]", contaNome);
+	getchar();
+    printf("///                        Digite sua senha:                      ///\n");
+	scanf("%[A-Z a-z 0-9]", contaSenha);
+	getchar();
+    printf("///                                                               ///\n");
+    printf("///                    Pressione 0 para voltar                    ///\n");
+    scanf("%i", &op4);
+    getchar();
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    return op4;
+}
+
+int menuEntrarConta (void) {
+	
+	char contaNome[20];
+	char contaSenha[20];
+	
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                               ///\n");
+    printf("///                        ******************                     ///\n");
+    printf("///                        ** Entrar Conta **                     ///\n");
+    printf("///                        ******************                     ///\n");
+    printf("///                                                               ///\n");
+    printf("///                        Digite seu nome (login):               ///\n");
+	scanf("%[A-Z a-z 0-9]", contaNome);
+	getchar();
+    printf("///                        Digite sua senha:                      ///\n");
+	scanf("%[A-Z a-z 0-9]", contaSenha);
+	getchar();
+    printf("///                                                               ///\n");
+    printf("///                    Pressione 0 para voltar                    ///\n");
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+  return 0;
+}
+
+
+
+
+//menu de resetar os dados
+int menuReset (void) {
+  char op5;
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                               ///\n");
+    printf("///                      *******************                      ///\n");
+    printf("///                      ** Resetar Conta **                      ///\n");
+    printf("///                      *******************                      ///\n");
+    printf("///                                                               ///\n");
+    printf("///       Tem certeza que deseja resetar os dados da conta?       ///\n");
+    printf("///                                                               ///\n");
+    printf("///                   (S)                     (N)                 ///\n");
+    printf("///                                                               ///\n");
+    printf("///                                                               ///\n");
+    printf("///                    Pressione 0 para voltar                    ///\n");
+    scanf("%char", &op5);
+    getchar();
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+  return op5;
+}
+
+//menu que mostra os placares (relatorios) (o quao rapido cada pessoa digitou)
+int menuPlacar (void) {
+  char op6;
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                               ///\n");
+    printf("///                          ************                         ///\n");
+    printf("///                          ** Placar **                         ///\n");
+    printf("///                          ************                         ///\n");
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("//////        CayoDantas           |         19.3s             //////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("//////        MatheusMed           |         24.5s             //////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("//////        JoaoVictor           |         30.2s             //////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("//////        EricMarley           |         32.1s             //////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("//////                             |                           //////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("//////                             |                           //////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("//////                             |                           //////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("//////                             |                           //////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");                            
+    printf("///                                                               ///\n");
+    printf("///                    Pressione 0 para voltar                    ///\n");
+    scanf("%char", &op6);
+    getchar();
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+  return 0;
+}
